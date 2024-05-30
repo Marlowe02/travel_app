87b7515d1c71b36aebda0df38cd36740b91a886c
Marlowe02 <154118075+Marlowe02@users.noreply.github.com>
2024-05-30 10:02:50 +0800

    added the resources and add the navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <h1 className="flex font-bold text-2xl justify-center items-center h-screen">marlowe</h1>;
+  return <h1 className="font-bold">marlowe</h1>;
 }
