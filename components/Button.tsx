becf3126a3ae53cf9588784bf9276bc334054db3
Marlowe02 <154118075+Marlowe02@users.noreply.github.com>
2024-06-01 09:50:57 +0800

    added the Navbar

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import Image from "next/image";
 
-const Button = () => {
-  return <button type="button">Button</button>;
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  variant: string;
+};
+
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+  return (
+    <button className={`flexCenter gap-3 rounded-full border ${variant}`} type={type}>
+      {icon && <Image src={icon} alt={title} height={24} width={24} />}
+      <label className="bold-16 whitespace-nowrap">{title}</label>
+    </button>
+  );
 };
 
 export default Button;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,10 +6,11 @@ import Button from "./Button";
 const Navbar = () => {
   return (
     <nav className=" flexBetween max-container padding-container relative z-30 py-5">
-      {/* LOGO */}
+      {/* hilink logo */}
       <Link href="/">
         <Image src="/hilink-logo.svg" height={29} width={74} alt="logo" />
       </Link>
+      {/* nav links */}
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
           <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
@@ -17,9 +18,11 @@ const Navbar = () => {
           </Link>
         ))}
       </ul>
+      {/* login button */}
       <div className="lg:flexCenter hidden">
-        <Button type="button" title="Login" icon />
+        <Button type="button" title="Login" icon="/user.svg" variant="btn_dark_green" />
       </div>
+      <Image src="menu.svg" alt="menu" height={32} width={32} className="inline-block cursor lg:hidden" />
     </nav>
   );
 };
